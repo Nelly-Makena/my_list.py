2f76218af2368f0dccc3dbc3478e9eecf542472b
Nelly Makena <nellysolomonmakena3@gmail.com>
2024-03-07 16:16:20 +0300

    main

---
+++ my_list.py
@@ -0,0 +1,26 @@
+my_list = []
+#appending
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+#output
+print(my_list)
+#inserting
+my_list.insert(1, 15)
+#Output
+print(my_list)
+#Extending
+my_list.extend([50,60,70])
+#output
+print(my_list)
+#pop
+my_list.pop()
+#output
+print(my_list)
+#sort
+my_list.sort()
+#Output
+print(my_list)
+index_30 = my_list.index(30)
+print(index_30)
